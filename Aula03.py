82608e8e48b7b8584fa30edd5a0ab80432975c88
alexml07 <alex.ml.07@gmail.com>
2025-10-31 19:55:44 -0300

    Acessando site do SENAI

--- Aula03.py
+++ Aula03.py
@@ -1,2 +1,12 @@
-print("Aula03")
-print("Operadores Aritméticos")
+import pyautogui as py
+
+py.press("win")
+py.write("chrome")
+py.press("enter")
+py.sleep(2)
+py.write("senai americana")
+py.press("enter")
+# py.sleep(5)
+# print(py.position())  # Posição do link do site
+py.sleep(2)
+py.click(x=246, y=462, button="left")  # Clique no link do site
