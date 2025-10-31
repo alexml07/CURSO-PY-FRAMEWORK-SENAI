92e3b6b31a3208115f5a66bd9827b69079fd7030
alexml07 <alex.ml.07@gmail.com>
2025-10-31 20:41:36 -0300

    Fim de aula

--- Aula03.py
+++ Aula03.py
@@ -10,3 +10,17 @@ py.press("enter")
 # print(py.position())  # Posição do link do site
 py.sleep(2)
 py.click(x=246, y=462, button="left")  # Clique no link do site
+# py.sleep(3)
+# print(py.position())
+py.sleep(2)
+py.click(x=1405, y=770, button="left")  # Clique no link do site
+# py.sleep(3)
+# print(py.position())
+py.sleep(2)
+py.click(x=241, y=501, button="left")
+py.sleep(1)
+py.write("python")
+py.sleep(1)
+py.press("enter")
+py.sleep(1)  # Rola a página para baixo
+py.vscroll(-500)  # Rola a página para baixo
