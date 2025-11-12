eca365cc3ff2fabf826c8701fec17e7a7da950bc
alexml07 <alex.ml.07@gmail.com>
2025-11-11 21:46:44 -0300

    251111 Fim de Aula

--- Aula10/pares_consecutivosST.py
+++ Aula10/pares_consecutivosST.py
@@ -6,7 +6,13 @@ st.title('Calculadora pares consecutivos')
 # Entrada de dados
 numero=st.number_input('Digite um numero inteiro', step=1)
 botao=st.button('Calcular')
+contagem=0
+soma=0
 if botao:
-    if (numero%2) != 0:
-        st.write('Impar')
-    else
+    if (numero%2)!=0:
+        numero+=1
+    while contagem<5:
+        soma+=numero
+        numero+=2
+        contagem+=1
+    st.write(soma)
